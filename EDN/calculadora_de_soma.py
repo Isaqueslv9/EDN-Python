3457897c15fd33906ef05de593f6cef436f7a141
Isaque Sebastiao <isaquedzn9@gmail.com>
2025-10-27 16:02:05 -0300

    Add files via upload

---
+++ EDN/calculadora_de_soma.py
@@ -0,0 +1,7 @@
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+
+print(f"O primeiro numero e: {numero1}")
+print(f"O segundo numero e: {numero2}")
+print(f"A soma dos numeros e: {soma}")
---
+++ EDN/calculadora_de_volume.py
@@ -0,0 +1,10 @@
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+
+
+print(f"Comprimento: {comprimento} cm")
+print(f"Largura: {largura} cm")
+print(f"Altura: {altura} cm")
+print(f"O volume da caixa e: {volume} cm³")
